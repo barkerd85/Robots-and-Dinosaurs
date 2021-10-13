130ceddfb91f138cf4270f916d7303a6d24a9346
Dustin Barker <barkerd85@yahoo.com>
2021-10-13 10:47:36 -0600

    Robot class created

--- Robot.py
+++ Robot.py
@@ -1,7 +1,9 @@
 
 
 class Robot:
-    def __init__(self):
-      pass
+    def __init__(self, name, weapon, health):
+      self.name = name
+      self.weapon =  weapon
+      self.health = health
 
 
