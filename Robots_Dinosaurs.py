397f32070ce6ce373097b4261c33aa4f91d05d64
Dustin Barker <barkerd85@yahoo.com>
2021-10-13 11:21:22 -0600

    Dinosaurs added

--- Robots_Dinosaurs.py
+++ Robots_Dinosaurs.py
@@ -8,5 +8,10 @@ atomic_launcher = Weapon('atomic launcher', 50)
 one_punch = Weapon('One punch', 5000)
 
 robot1 = Robot('T1000', 300, laser)
-robot2 = Robot('T5000', 400, atomic_launcher)
+robot2 = Robot('JD5000', 400, atomic_launcher)
 robot3 = Robot('DB-7500', 1000, one_punch)
+
+
+dino1 = Dinosaur('T-rex', 350, 15)
+dino2 = Dinosaur('Raptor', 450, 100)
+dino3= Dinosaur('Hybrid', 800, 4000)
