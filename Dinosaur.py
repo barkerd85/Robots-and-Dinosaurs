bf68138b0bf05a6155da5f4730257c5f22bd97fb
Dustin Barker <barkerd85@yahoo.com>
2021-10-13 10:52:10 -0600

    Dinosaur class created

--- Dinosaur.py
+++ Dinosaur.py
@@ -1,8 +1,11 @@
 
 
 class Dinosaur:
-    def __init__(self):
-      pass
+    def __init__(self, name, health, attack_power):
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power
+
 
 
 
